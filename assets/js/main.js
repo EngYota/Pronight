08cf2d6dc7a6dbc4676256186fe37b82973c20cf
El3shry <ayanada816@gmail.com>
2025-04-30 02:29:52 +0300

    Home

---
+++ assets/js/main.js
@@ -0,0 +1,132 @@
+
+(function($) {
+    "use strict";
+
+    $(document).on('ready', function() {
+
+
+		/* ==================================================
+			# Data Background
+		 ===============================================*/
+
+		$("[data-background]").each(function(){
+			$(this).css("background-image","url(" + $(this).attr("data-background") +")");
+		});
+
+        /* ==================================================
+            # Wow Init
+         ===============================================*/
+        var wow = new WOW({
+            boxClass: 'wow', // animated element css class (default is wow)
+            animateClass: 'animated', // animation css class (default is animated)
+            offset: 0, // distance to the element when triggering the animation (default is 0)
+            mobile: true, // trigger animations on mobile devices (default is true)
+            live: true // act on asynchronously loaded content (default is true)
+        });
+        wow.init();
+	
+		
+		
+        /* ==================================================
+            # Smooth Scroll
+         =============================================== */
+        
+        $('a.smooth-menu').on('click', function(event) {
+            var $anchor = $(this);
+            var headerH = '85';
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - headerH + "px"
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+		
+
+		 /* ==================================================
+            # Scroll to top
+         =============================================== */
+			//Get the button
+			var mybutton = document.getElementById("scrtop");
+
+			// When the user scrolls down 20px from the top of the document, show the button
+			window.onscroll = function() {scrollFunction()};
+
+			function scrollFunction() {
+			  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+				mybutton.style.display = "block";
+			  } else {
+				mybutton.style.display = "none";
+			  }
+			}
+	
+		/* ==================================================
+			# Accordion Menu
+		 =============================================== */
+		
+		  $(document).on('click','.panel-group .panel',function(e) {
+			e.preventDefault();
+			$(this).addClass('panel-active').siblings().removeClass('panel-active');
+		});
+	
+		
+		
+        
+       
+
+    
+		
+
+		
+	
+		
+        /* ==================================================
+            # Fun Factor Init
+        ===============================================*/
+        $('.timer').countTo();
+        $('.fun-fact').appear(function() {
+            $('.timer').countTo();
+        }, {
+            accY: -100
+        });
+
+
+        /* ==================================================
+            Preloader Init
+         ===============================================*/
+        $(window).on('load', function() {
+            // Animate loader off screen
+            $(".se-pre-con").fadeOut("200000s");
+        });
+        
+		
+	
+		 /* ==================================================
+            Mouse Animation
+        ================================================== */
+
+			function theme_tm_cursor(){
+
+				var myCursor	= jQuery('.mouse-cursor');
+
+				if(myCursor.length){
+					if ($("body")) {
+					const e = document.querySelector(".cursor-inner"),
+						t = document.querySelector(".cursor-outer");
+					let n, i = 0,
+						o = !1;
+					window.onmousemove = function (s) {
+						o || (t.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)"), e.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)", n = s.clientY, i = s.clientX
+					}, $("body").on("mouseenter", "a, .cursor-pointer", function () {
+						e.classList.add("cursor-hover"), t.classList.add("cursor-hover")
+					}), $("body").on("mouseleave", "a, .cursor-pointer", function () {
+						$(this).is("a") && $(this).closest(".cursor-pointer").length || (e.classList.remove("cursor-hover"), t.classList.remove("cursor-hover"))
+					}), e.style.visibility = "visible", t.style.visibility = "visible"
+				}
+				}
+			};
+			theme_tm_cursor()	
+		
+
+
+		
+    }); // end document ready function
+})(jQuery); // End jQuery
