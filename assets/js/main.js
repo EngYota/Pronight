6eade6febc827f42c9c7c33877854414f9522c41
El3shry <ayanada816@gmail.com>
2025-05-07 13:46:20 +0300

    register

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,7 +97,23 @@
             $(".se-pre-con").fadeOut("200000s");
         });
         
-		
+		/*----------------------------------
+     upload image
+-----------------------------------*/
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            $('#imagePreview').css('background-image', 'url('+e.target.result +')');
+            $('#imagePreview').hide();
+            $('#imagePreview').fadeIn(650);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+$("#imageUpload").change(function() {
+    readURL(this);
+});
 	
 		 /* ==================================================
             Mouse Animation
